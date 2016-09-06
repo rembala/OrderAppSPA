1f7d95377a1219c8a7c04b535efa5ccd60dd6f65
Artur <maslovskij.artur@gmail.com>
2016-09-06 21:15:42 +0300

    some changes in controllers

---
+++ Orders/Scripts/spa/Product/productCtrl.js
@@ -0,0 +1,32 @@
+﻿(function (app) {
+    app.controller('productCtrl', productCtrl);
+
+    productCtrl.$inject = ['$scope', 'apiService', 'notificationService', '$modal', '$location'];
+
+    function productCtrl($scope, apiService, notificationService, $modal, $location) {
+        $scope.loadProductData = loadProductData;
+        $scope.Products = [];
+
+        function loadProductData() {
+            apiService.get('api/Product', null, ProductDataSuccesfull, ProductDataFailure);
+        }
+
+        function ProductDataSuccesfull(result) {
+            $scope.Products = result.data;
+        }
+
+        function ProductDataFailure() {
+
+        }
+
+        function selectProductTypeFn($item) {
+            if ($item) {
+                $scope.CountryId = $item.originalObject.CountryID;
+            } else {
+                $scope.CountryId = null;
+            }
+        }
+        loadProductData();
+    }
+
+})(angular.module('Order'))
