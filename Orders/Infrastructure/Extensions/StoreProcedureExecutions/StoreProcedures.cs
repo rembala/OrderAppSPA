2351f88f46dba959b38d38e0a7a74e227886f049
Artur <maslovskij.artur@gmail.com>
2016-09-07 19:31:51 +0300

    SP comment

--- Orders/Infrastructure/Extensions/StoreProcedureExecutions/StoreProcedures.cs
+++ Orders/Infrastructure/Extensions/StoreProcedureExecutions/StoreProcedures.cs
@@ -20,13 +20,29 @@ namespace Orders.Infrastructure.Extensions.StoreProcedureExecutions
                 new SqlParameter("@Filter", string.IsNullOrEmpty(Filter) ? (object)DBNull.Value : Filter))).ToList();
             //Reikia susikurti procedura pries executinant sita.
         }
-
+        /// <summary>
+        /// Produkto pagal uzsakymo id gavimas
+        /// </summary>
+        /// <param name="OrderId"></param>
+        /// <returns></returns>
         public List<OrderProductViewModel> ProductOrderGet(int? OrderId)
         {
             return (Database.SqlQuery<OrderProductViewModel>("EXEC [web].[ProductOrder_Get] @OrderId",
             new SqlParameter("@OrderId", !OrderId.HasValue ? (object)DBNull.Value : OrderId))).ToList();
         }
-
+        /// <summary>
+        /// Uzsakymo saugojimas
+        /// </summary>
+        /// <param name="OrderId"></param>
+        /// <param name="OrderTime"></param>
+        /// <param name="UserId"></param>
+        /// <param name="OrderNo"></param>
+        /// <param name="StatusId"></param>
+        /// <param name="CountryId"></param>
+        /// <param name="ClientId"></param>
+        /// <param name="PlannedDate"></param>
+        /// <param name="IsActive"></param>
+        /// <returns></returns>
         public decimal Order_Save(int? OrderId, DateTime OrderTime, int UserId, int OrderNo, int StatusId, int CountryId, int ClientId, DateTime PlannedDate, bool IsActive)
         {
             return (Database.SqlQuery<decimal>("EXEC [web].[Order_Save] @OrderId,@OrderTime,@UserID,@OrderNo,@StatusID,@CountryID,@ClientID,@PlannedDate,@IsActive",
